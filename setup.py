b0e9ccca902b1532b362e84a0fb2ec2b5c3e4410
redsuit <grgbls647@gmail.com>
2020-07-06 14:50:04 +0000

    Replace setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
     name = 'hwrk',
     packages=["hwrk"],
-    version = '0.1',
+    version = '0.3',
     description = 'simple script to not forget about your homework',
+    long_description = 'because writing your homework on a piece of paper is too mainstream',
     author = 'GrgBls',
     url = 'https://gitlab.com/GrgBls/hwrk',
     py_modules=['hwrk'],
